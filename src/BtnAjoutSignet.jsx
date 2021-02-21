c549d86efb72bf4f10eb101ed5faccc3b6870775
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-21 15:29:39 -0500

    Ajout Material-Ui Icon + Core + FAB

--- src/BtnAjoutSignet.jsx
+++ src/BtnAjoutSignet.jsx
@@ -1,9 +1,10 @@
 import './BtnAjoutSignet.scss';
+import AddIcon from '@material-ui/icons/Add';
 
 export default function BtnAjoutSignet() {
   return (
       <div className="BtnAjoutSignet">
-        +
+        <AddIcon/>
       </div>
   );
 }
--- src/Signets.jsx
+++ src/Signets.jsx
@@ -1,14 +1,19 @@
 import './Signets.scss';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SortIcon from '@material-ui/icons/Sort';
+import { Fab } from '@material-ui/core';
 
 export default function Signets(props) {
   return (
     <li className="Signets">
-      <div className="iconTri">X</div>
+      <div className="iconTri">
+        <Fab size="small"> <SortIcon /> </Fab>
+      </div>
       <img src={'signets/' + props.id + '.jpg'} alt={props.titre}/>
         <div className="info"> 
           <h1 className="titre">{props.titre}</h1>
           <p className="prix">Modifié : {props.date}</p>
-          <div className="btnPlus">+</div>
+          <div className="btnPlus">< MoreVertIcon /></div>
       </div>
     </li>
   );
