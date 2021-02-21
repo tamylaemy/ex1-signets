5bacb01317159a8d8d74116302cdb9b02c97d565
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-21 14:24:55 -0500

    Ajout media-queries signet height + width

--- src/Signets.jsx
+++ src/Signets.jsx
@@ -4,7 +4,7 @@ export default function Signets(props) {
   return (
     <li className="Signets">
       <div className="iconTri">X</div>
-      <img src={'signets/' + props.id + '.jpg'} alt={props.titre} height="200px" width="300px" />
+      <img src={'signets/' + props.id + '.jpg'} alt={props.titre}/>
         <div className="info"> 
           <h1 className="titre">{props.titre}</h1>
           <p className="prix">Modifié : {props.date}</p>
