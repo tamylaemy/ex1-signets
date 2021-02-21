23cd44271378d9409eb6ae517ed3465cd871afcd
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-21 15:58:23 -0500

    Avatar de la personne connecté

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -1,4 +1,5 @@
 import './Entete.scss';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function Entete(props) {
 return (
@@ -6,7 +7,7 @@ return (
         <div className="Logo">Signets (Beta)</div>
         <div className="Login">
             <h1 className="Nom"> Tamyla A-C</h1>
-            <img src="avatar/dinosaur-avatar.jpg" alt="Avatar de la personne connecté" className="Avatar"/>
+            <AccountCircleIcon className="Avatar"/>
         </div>
    </header>
 )
