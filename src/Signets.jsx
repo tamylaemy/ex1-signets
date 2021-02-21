b81d0e6e916a883cbd2f9864e721be1713e71a47
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-21 13:18:14 -0500

    SCSS signet ajout taille

--- src/Signets.jsx
+++ src/Signets.jsx
@@ -3,13 +3,13 @@ import './Signets.scss';
 export default function Signets(props) {
   return (
     <li className="Signets">
-    <div className="iconTri">X</div>
-    <img src={'signets/' + props.id + '.jpg'} alt={props.titre} height="200px" width="300px" />
-      <div className="info"> 
-        <h1 className="titre">{props.titre}</h1>
-        <p className="prix">Modifié : {props.date}</p>
-        <div className="btnPlus">+</div>
+      <div className="iconTri">X</div>
+      <img src={'signets/' + props.id + '.jpg'} alt={props.titre} height="200px" width="300px" />
+        <div className="info"> 
+          <h1 className="titre">{props.titre}</h1>
+          <p className="prix">Modifié : {props.date}</p>
+          <div className="btnPlus">+</div>
       </div>
-  </li>
+    </li>
   );
 }
