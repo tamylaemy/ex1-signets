438ea3d5f6f89df32afa5146aa45c0fcb0da592a
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-24 18:43:50 -0500

    Ajout FAB plus en bas à droite

--- src/BtnAjoutDossier.jsx
+++ src/BtnAjoutDossier.jsx
@@ -1,10 +1,11 @@
 import './BtnAjoutDossier.scss';
 import AddIcon from '@material-ui/icons/Add';
+import { Fab } from '@material-ui/core';
 
 export default function BtnAjoutDossier() {
   return (
       <div className="BtnAjoutDossier">
-        <AddIcon/>
+        <Fab size="small" className="btnPlusDossier"> <AddIcon/> </Fab>
       </div>
   );
 }
